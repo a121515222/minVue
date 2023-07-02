37b8ba31962664f9643503b49b37be02b8e8d556
CHUN-CHIA <a121515222@gmail.com>
2023-07-02 22:59:47 +0800

    finsh scheduler

--- src/test/effect.spec.ts
+++ src/test/effect.spec.ts
@@ -16,7 +16,7 @@ describe("effect", () => {
     user.age++;
     expect(nextAge).toBe(12);
   });
-  it("should effect return runner when effect",()=>{
+  it("should return runner when call effect",()=>{
     let foo = 10;
     const runner = effect(()=>{
       foo ++
@@ -26,5 +26,29 @@ describe("effect", () => {
     const r = runner();
     expect(foo).toBe(12);
     expect(r).toBe(foo);
+  });
+  it("scheduler",()=>{
+    //通過effect的第二個參數給定一個scheduler
+    //effect第一次執行的時候會執行fn
+    //當響應式對象set update不會執行fn 而是執行scheduler
+    //如果當執行runner時會再次執行fn
+    let dummy;
+    let run: any;
+    const scheduler = jest.fn(()=>{
+      run = runner;
+    });
+    const obj = reactive({ foo:1 });
+    const runner = effect(
+     ()=>{
+      dummy = obj.foo;
+     },{ scheduler } 
+    );
+    expect(scheduler).not.toHaveBeenCalled();
+    expect(dummy).toBe(1);
+    obj.foo++;
+    expect(scheduler).toHaveBeenCalledTimes(1);
+    expect(dummy).toBe(1);
+    run();
+    expect(dummy).toBe(2);
   })
 });
