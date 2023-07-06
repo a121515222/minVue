2d366b29f7542810cc10b0b7a8bd0f864794bf3e
CHUN-CHIA <a121515222@gmail.com>
2023-07-07 00:23:13 +0800

    finshed readonly

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,17 +1,38 @@
 import { track, trigger } from "./effect";
 
-export function reactive(raw) {
-  return new Proxy(raw, {
-    get(target, key) {
-      const res = Reflect.get(target, key);
-      // 依賴收集
+
+function createGetter(isReadonly = false) {
+  return function get (target, key){
+    const res = Reflect.get(target, key);
+    // 依賴收集
+    if(!isReadonly) {
       track(target, key);
-      return res;
-    },
-    set(target, key, value) {
-      const res = Reflect.set(target, key, value);
+    }
+    return res;
+  }
+}
+
+function createSetter() {
+  return function set (target, key, value) {
+    const res = Reflect.set(target, key, value);
       trigger(target, key);
       return res;
+  }
+}
+
+
+export function reactive(raw) {
+  return new Proxy(raw, {
+    get: createGetter(),
+    set: createSetter(),
+  });
+}
+
+export function readonly(raw) {
+  return new Proxy(raw, {
+    get: createGetter(),
+    set(target, key, value) {
+      return true;
     },
   });
 }
