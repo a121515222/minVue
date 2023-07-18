6200d37a6817841e944e20b7c714f9b1a4bf0b49
CHUN-CHIA <a121515222@gmail.com>
2023-07-18 23:44:22 +0800

    add the nested readonly and reactive

--- src/reactivity/baseHandler.ts
+++ src/reactivity/baseHandler.ts
@@ -1,17 +1,22 @@
+import { isObject } from "../shared";
 import { track, trigger } from "./effect";
-import { ReactiveFlags } from "./reactive";
+import { ReactiveFlags, reactive, readonly } from "./reactive";
 const get = createGetter();
 const set = createSetter();
 const readonlyGet = createGetter(true);
 
 function createGetter(isReadonly = false) {
   return function get(target, key) {
-    const res = Reflect.get(target, key);
     if (key === ReactiveFlags.IS_REACTIVE) {
       return !isReadonly;
     } else if (key === ReactiveFlags.IS_READONLY) {
       return isReadonly;
     }
+    const res = Reflect.get(target, key);
+    if (isObject(res)) {
+      return isReadonly ? readonly(res) : reactive(res);
+    }
+
     // 依賴收集
     if (!isReadonly) {
       track(target, key);
--- src/test/readonly.spec.ts
+++ src/test/readonly.spec.ts
@@ -1,20 +1,21 @@
 import { isReadonly, readonly } from "../reactivity/reactive";
-describe("readonly",()=>{
-
-  it("happy path", ()=>{
-    const original = { foo:1, bar: { baz:2} };
+describe("readonly", () => {
+  it("happy path", () => {
+    const original = { foo: 1, bar: { baz: 2 } };
     const wrapped = readonly(original);
-    
+
     expect(wrapped).not.toBe(original);
-    expect(wrapped.foo).toBe(1);
     expect(isReadonly(wrapped)).toBe(true);
     expect(isReadonly(original)).toBe(false);
+    expect(isReadonly(wrapped.bar)).toBe(true);
+    expect(isReadonly(original.bar)).toBe(false);
+    expect(wrapped.foo).toBe(1);
   });
 
-  it("warn then call set", () =>{
+  it("warn then call set", () => {
     console.warn = jest.fn();
     const user = readonly({
-      age:10
+      age: 10,
     });
     user.age = 11;
 
