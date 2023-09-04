2ec0536a776c6c76e9464290b516bce687cc53e4
CHUN-CHIA <a121515222@gmail.com>
2023-09-04 14:12:36 +0800

    finished init element process

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,3 +1,4 @@
+import { isObject } from "../shared/index";
 import { createComponentInstance, setupComponent } from "./component";
 export function render(vnode, container) {
   // 調用patch
@@ -8,7 +9,41 @@ function patch(vnode, container) {
   //判斷是不是element
   // to do 判斷vnode是不是element
   // processElement
-  processComponent(vnode, container);
+  console.log(typeof vnode);
+  if (typeof vnode.type === "string") {
+    processElement(vnode, container);
+  } else if (isObject(vnode.type)) {
+    processComponent(vnode, container);
+  }
+}
+
+function processElement(vnode: any, container: any) {
+  mountElement(vnode, container);
+}
+
+function mountElement(vnode: any, container: any) {
+  const el = document.createElement(vnode.type);
+
+  // 收到vnode是 string or array
+  const { children } = vnode;
+  if (typeof children === "string") {
+    el.textContent = children;
+  } else if (Array.isArray(children)) {
+    mountChildren(vnode, el);
+  }
+
+  // props
+  const { props } = vnode;
+  for (const key in props) {
+    const val = props[key];
+    el.setAttribute(key, val);
+  }
+  container.append(el);
+}
+function mountChildren(vnode, container) {
+  vnode.children.forEach((v) => {
+    patch(v, container);
+  });
 }
 
 function processComponent(vnode: any, container: any): void {
