edc51dbe4f7cbe1c1b4e34ba54b953d3f5c20d89
a121515222 <a121515222@hotmail.com>
2024-02-06 16:17:25 +0800

    finished element parse

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,5 +1,9 @@
 import { NodeTypes } from "./ast";
 
+const enum TagType {
+  Start,
+  End,
+}
 
 export function baseParse(content:string) {
 
@@ -10,13 +14,44 @@ export function baseParse(content:string) {
 function parseChildren (context) {
   const nodes : any= [];
   let node;
-  if(context.source.startsWith("{{")){
+  const s = context.source;
+  if(s.startsWith("{{")){
     node = parseInterpolation(context);
+  } else if(s[0] ==="<") {
+    if(/[a-z]/i.test(s[1])){
+      console.log("parse element")
+      node =  parseElement(context);
+    }
   }
   nodes.push(node);
   return nodes
 
 
+}
+function parseElement(context: any) {
+
+  //1.解析tag
+  const element =  parseTag(context,TagType.Start);
+  parseTag(context,TagType.End);
+  console.log("-------",context.source)
+  return element
+}
+function parseTag(context: any, type:TagType) {
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source);
+  console.log("match", match);
+  const tag = match[1];
+  //2.刪除解析後的context 簡單的來說處理<div></div>
+  // 處理<div
+  advanceBy(context, match[0].length);
+  // 處理>
+  advanceBy(context, 1);
+  // 處理</div>
+  if(type === TagType.End) return;
+  console.log("context", context.source);
+  return {
+    type: NodeTypes.ELEMENT,
+    tag,
+  };
 }
 
 function parseInterpolation(context){
@@ -64,3 +99,5 @@ function createParserContext(content:string):any{
     source:content
   }
 }
+
+
